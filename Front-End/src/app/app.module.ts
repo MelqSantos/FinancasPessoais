2c8dbb491b188ec431b93e9320b3530eee6c0214
Melqui Santos <melqsantos96@gmail.com>
2022-12-12 18:29:33 -0300

    feat: ajuste nas configs

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { OrderModule } from 'ngx-order-pipe';
 import { CurrencyMaskModule } from 'ng2-currency-mask';
 import { GaugeModule } from 'angular-gauge';
 import { NgChartsModule } from 'ng2-charts';
-import { GaugeChartModule } from 'angular-gauge-chart';
+// import { GaugeChartModule } from 'angular-gauge-chart';
 import ptBr from '@angular/common/locales/pt';
 import { registerLocaleData } from '@angular/common';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
@@ -29,7 +29,7 @@ import { PerfilComponent } from './paginas/perfil/perfil.component';
 import { EconomiasComponent } from './paginas/economias/economias.component';
 import { GraficoComponent } from './shared/grafico/grafico.component';
 import { TransacoesComponent } from './paginas/transacoes/transacoes.component';
-import { GaugeChartComponent } from './shared/gauge-chart/gauge-chart.component';
+// import { GaugeChartComponent } from './shared/gauge-chart/gauge-chart.component';
 import { RelatoriosComponent } from './paginas/relatorios/relatorios.component';
 import { InterceptorService } from './paginas/components/interceptor.service';
 
@@ -45,7 +45,7 @@ import { InterceptorService } from './paginas/components/interceptor.service';
     EconomiasComponent,
     GraficoComponent,
     TransacoesComponent,
-    GaugeChartComponent,
+    // GaugeChartComponent,
     RelatoriosComponent,
   ],
   imports: [
@@ -57,9 +57,9 @@ import { InterceptorService } from './paginas/components/interceptor.service';
     MatIconModule,
     OrderModule,
     CurrencyMaskModule,
-    GaugeModule,
+    // GaugeModule,
     NgChartsModule,
-    GaugeChartModule,
+    // GaugeChartModule,
     MatProgressBarModule,
     ToastrModule.forRoot(),
     ModalModule.forRoot(),
