4b8116f9a1b96cf51fab7d45446beb0c7c37c561
Melqui Santos <melqsantos96@gmail.com>
2022-12-12 19:23:56 -0300

    feat: ajustes configs

--- Front-End/src/app/shared/gauge-chart/gauge-chart.component.ts
+++ Front-End/src/app/shared/gauge-chart/gauge-chart.component.ts
@@ -1,31 +1,31 @@
-// import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-gauge-chart',
-//   templateUrl: './gauge-chart.component.html',
-//   styleUrls: ['./gauge-chart.component.css']
-// })
-// export class GaugeChartComponent implements OnInit {
+@Component({
+  selector: 'app-gauge-chart',
+  templateUrl: './gauge-chart.component.html',
+  styleUrls: ['./gauge-chart.component.css']
+})
+export class GaugeChartComponent implements OnInit {
 
-//   perc = Number(sessionStorage.getItem('percentual'))
+  perc = Number(sessionStorage.getItem('percentual'))
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit() {}
+  ngOnInit() {}
 
-//   public canvasWidth = 300
-//   public needleValue = this.perc
-//   public centralLabel = ''
-//   public name = 'Controle financeiro'
-//   public bottomLabel = this.perc + '%'
-//   public options = {
-//     hasNeedle: true,
-//     needleColor: '#343A40',
-//     needleUpdateSpeed: 1000,
-//     arcColors: ['rgb(255,84,84)', 'rgb(239,214,19)', 'rgb(61,204,91)'],
-//     arcDelimiters: [30, 70],
-//     rangeLabel: ['0', '100'],
-//     needleStartValue: 50,
-//   }
+  public canvasWidth = 300
+  public needleValue = this.perc
+  public centralLabel = ''
+  public name = 'Controle financeiro'
+  public bottomLabel = this.perc + '%'
+  public options = {
+    hasNeedle: true,
+    needleColor: '#343A40',
+    needleUpdateSpeed: 1000,
+    arcColors: ['rgb(255,84,84)', 'rgb(239,214,19)', 'rgb(61,204,91)'],
+    arcDelimiters: [30, 70],
+    rangeLabel: ['0', '100'],
+    needleStartValue: 50,
+  }
 
-// }
+}
